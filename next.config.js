1112f516cd2b02d8796fd16e769dd63b495248ae
Missi Cherifi <missi@MacBook-Air-de-Missi.local>
2025-06-02 09:29:42 +0200

    ajout https

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  poweredByHeader: false,
+  compress: true,
+  images: {
+    domains: ['monvtc37.fr'], // Ajoutez ici votre domaine
+    minimumCacheTTL: 60,
+  },
 }
 
 module.exports = nextConfig 
